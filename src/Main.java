a594882ff0bf4db0351c4e5e966affe4b859f8da
jihoho12 <kjh235235@gmail.com>
2025-11-28 22:47:15 +0900

    기능: 학생 목록 출력 메소드 추가

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git Practice!");
         System.out.println("Github 연동 성공! 기능 추가 중...");
+
+        studentList(); // 새로 추가한 기능 호출
+    }
+
+    // 새로 만든 기능
+    public static void studentList() {
+        System.out.println("=== 학생 목록 ===");
+        System.out.println("1. 철수");
+        System.out.println("2. 영희");
     }
 }
