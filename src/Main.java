4727910b0f4896dc35df357e0d9b544d1ab10abd
jihoho12 <kjh235235@gmail.com>
2025-11-28 22:35:49 +0900

    기능 추가: 성공 메시지 출력 코드 추가

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-// Main.java 내용
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git Practice!");
+        System.out.println("Github 연동 성공! 기능 추가 중...");
     }
 }
