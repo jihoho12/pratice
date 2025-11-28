83376676f0908c7759bf9158de32d519979663e3
jihoho12 <kjh235235@gmail.com>
2025-11-28 22:56:05 +0900

    수정: 메인에서 인사말 변경

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git Practice!");
+        System.out.println("Hello, Main World!"); // 똑같은 위치를 다르게 수정!
         System.out.println("Github 연동 성공! 기능 추가 중...");
 
         studentList(); // 새로 추가한 기능 호출
